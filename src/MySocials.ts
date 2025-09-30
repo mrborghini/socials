92762cff65f4536e815f292954333f38e0529c9a
mrborghini <61702082+mrborghini@users.noreply.github.com>
2025-09-30 23:35:38 +0200

    feat: add kofi

--- src/MySocials.ts
+++ src/MySocials.ts
@@ -4,41 +4,51 @@ import Youtube from '/youtube.svg';
 import Discord from '/discord.svg';
 import Twitch from '/twitch.svg';
 import Github from '/github.svg';
+import Kofi from '/kofi.svg'
 
-const socials: SocialInfo[] = [{
-    id: 5,
-    title: "mrborghini_",
-    url: "https://twitter.com/mrborghini_",
-    image: Twitter,
-    alt: "twitter-logo"
-},
-{
-    id: 3,
-    title: "mrborghini",
-    url: "https://www.youtube.com/channel/UCnRW7gN7dpmD7rCixHKHXfw",
-    image: Youtube,
-    alt: "youtube-logo"
-}, {
-    id: 4,
-    title: "Lambo community server",
-    url: "https://discord.gg/z48t39mW27",
-    image: Discord,
-    alt: "discord-logo"
-},
-{
-    id: 2,
-    url: "https://www.twitch.tv/mrborghini_",
-    title: "mrborghini_",
-    image: Twitch,
-    alt: "twitch-logo"
-},
-{
-    id: 1,
-    url: "https://github.com/mrborghini",
-    title: "mrborghini",
-    image: Github,
-    alt: "github-logo"
-}
+const socials: SocialInfo[] = [
+    {
+        id: 6,
+        title: "Buy me a coffee",
+        url: "https://ko-fi.com/mrborghini",
+        image: Kofi,
+        alt: "kofi-logo"
+    },
+    {
+        id: 5,
+        title: "mrborghini_",
+        url: "https://twitter.com/mrborghini_",
+        image: Twitter,
+        alt: "twitter-logo"
+    },
+    {
+        id: 4,
+        title: "Lambo community server",
+        url: "https://discord.gg/z48t39mW27",
+        image: Discord,
+        alt: "discord-logo"
+    },
+    {
+        id: 3,
+        title: "mrborghini",
+        url: "https://www.youtube.com/channel/UCnRW7gN7dpmD7rCixHKHXfw",
+        image: Youtube,
+        alt: "youtube-logo"
+    },
+    {
+        id: 2,
+        url: "https://www.twitch.tv/mrborghini_",
+        title: "mrborghini_",
+        image: Twitch,
+        alt: "twitch-logo"
+    },
+    {
+        id: 1,
+        url: "https://github.com/mrborghini",
+        title: "mrborghini",
+        image: Github,
+        alt: "github-logo"
+    }
 ];
 
 function sortSocials(socials: SocialInfo[]) {
